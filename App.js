5877aa85b3ecda90c8494fa674a1a81230349642
SAbbaszada01 <sultanabbas371@gmail.com>
2025-06-28 13:21:58 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CVForm from './components/CVForm';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Azərbaycan CV Generatoru</h1>
+      <CVForm />
+    </div>
+  );
+}
+
+export default App;
